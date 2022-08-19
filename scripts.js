050fbfb399d7656f46b06e0aff3386163c8009e3
Nabil <holanabilkhalid@gmail.com>
2022-08-19 13:35:32 +0800

    Added game function

--- scripts.js
+++ scripts.js
@@ -17,9 +17,7 @@ function getComputerChoice() {
 
 // Function that decides who's the winner by conditional statements
 
-function playRound(playerSelection, computerSelection) {
-    let input = playerSelection.toLowerCase();
-
+function playRound(playerSelection, computerSelection) { 
     if(playerSelection === computerSelection) { 
         return "It's a draw.";
     }
@@ -35,11 +33,33 @@ function playRound(playerSelection, computerSelection) {
     } 
 };
 
-const playerSelection = prompt("Choose Your Weapon?", "Rock, Paper or Scissors");
+const userInput = prompt("Choose Your Weapon?", "Rock, Paper or Scissors");
+const playerSelection = userInput.toLowerCase();
 const computerSelection = getComputerChoice();
 
 // const computerSelection = prompt ("Computer, make your move!");
 
-console.log(computerSelection);
+console.log(`You throws ${playerSelection}`);
+console.log(`Computer throws ${computerSelection}`);
 console.log(playRound(playerSelection, computerSelection));
+
+
+const playround = playRound();
+
+function game() {
+    let computerScore = 0;
+    let userScore = 0;
+    let round = 5;
+    for (let i = 0; i < round; i++){
+      if (playround === "Computer Won"){
+        return `Computer - ${computerScore += 1}` ;
+      } else if (playround === "It's a draw.") {
+        return `Play again!`;
+      } else {
+        return `User - ${userScore += 1}`;
+      }
+    }
+}
+
+console.log(game());
 
