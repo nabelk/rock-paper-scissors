9712369add840f4894fc498ff4d5eebf5604326e
Nabil <holanabilkhalid@gmail.com>
2022-08-19 15:54:13 +0800

    Fix game function issue

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+const playerSelection = prompt("Choose Your Weapon?", "Rock, Paper or Scissors");
+const computerSelection = getComputerChoice();
+
 // Function to get a computer choice by randomly.
 
 function getComputerChoice() {
@@ -13,7 +16,7 @@ function getComputerChoice() {
             return "paper";
             break;        
     }
-} 
+}; 
 
 // Function that decides who's the winner by conditional statements
 
@@ -22,44 +25,33 @@ function playRound(playerSelection, computerSelection) {
         return "It's a draw.";
     }
 
-    if (playerSelection === "rock" && computerSelection === "scissors") {
+    if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
         return "You Won! Rock beats Scissors";
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
         return "You won! Scissors beats Paper";
-    } else if (playerSelection === "paper" && computerSelection === "rock"){
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock"){
         return "You Won! Paper beats Rock";
     } else {
         return "Computer Won";
     } 
 };
 
-const userInput = prompt("Choose Your Weapon?", "Rock, Paper or Scissors");
-const playerSelection = userInput.toLowerCase();
-const computerSelection = getComputerChoice();
-
-// const computerSelection = prompt ("Computer, make your move!");
-
-console.log(`You throws ${playerSelection}`);
-console.log(`Computer throws ${computerSelection}`);
-console.log(playRound(playerSelection, computerSelection));
-
-
-const playround = playRound();
-
+const playround =  playRound(playerSelection, computerSelection);
 function game() {
-    let computerScore = 0;
-    let userScore = 0;
-    let round = 5;
-    for (let i = 0; i < round; i++){
-      if (playround === "Computer Won"){
-        return `Computer - ${computerScore += 1}` ;
-      } else if (playround === "It's a draw.") {
-        return `Play again!`;
-      } else {
-        return `User - ${userScore += 1}`;
-      }
-    }
-}
+    let computerScore = 1;
+    let userScore = 1;
+    for (let i = 1; i < 2; i++){
+            if (playround === "Computer Won"){
+              return "Computer:" + computerScore  ;
+            } else if (playround === "It's a draw.") {
+              return `Play again!`;
+            } else {
+              return "You:" + userScore;
+            }
+          }
+      };
 
+console.log(`You throws ${playerSelection.toLowerCase()}`);
+console.log(`Computer throws ${computerSelection}`);
+console.log(playRound(playerSelection, computerSelection));      
 console.log(game());
-
