d48a16f83e899f5b8c0ac6a1179701893fb12304
Nabil <holanabilkhalid@gmail.com>
2022-09-18 01:20:50 +0800

    Fix the whole the structures to make it work with dom

--- scripts.js
+++ scripts.js
@@ -1,17 +1,13 @@
-// Function to get a user choice
-
-/* function getUserChoice() {
-    let userInput = prompt("Choose Your Weapon?", "Rock, Paper or Scissors");
-    return userInput.toLowerCase();
-} */
-
+// Play again buttton
+function refresh(){
+    window.location.reload("Play Again")
+  }
 
 // Add event click to the player input button
 
-const button = document.querySelectorAll(".button");
+const button = document.querySelectorAll("button");
 button.forEach(btn => {
     btn.addEventListener("click", playRound)
-    // btn.addEventListener("click", finalScore)
 })
 
 // Function to get a computer choice by randomly."
@@ -31,110 +27,75 @@ function getComputerChoice() {
     }
 }; 
 
-// Function that decides who's the winner by conditional statements
+// Lines of code where the games starts
+
+let userScore = 0;
+let computerScore = 0;
+alert("Welcome to Rock, Paper and Scissors! First 5 points to win. Good Luck!")
+let userName = prompt("Your Name");
+userName = userName.charAt(0).toUpperCase() + userName.slice(1).toLowerCase();
+document.getElementById("userName").textContent = userName;
 
 function playRound(e) { 
-    // user Input
+    // User Input to print on p.Result element
     const playerSelection = e.target.value;
     const playerInput = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-    // computer random Input
+
+    // Computer random Input
     const computerSelection = getComputerChoice();
     const compInput = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
     
-    // Check the winner
+    // .textContent what ther player throws
     const divResult = document.querySelector(".result")
     divResult.textContent = `You throws ${playerInput}.\nComputer throws ${compInput}`
     
     const pResult = document.querySelector("#result");
-
-        if( playerSelection === computerSelection){
-            pResult.textContent =  "It's a draw";
-        } else if (e.target.classList.contains("rock") && computerSelection === "scissors") {
-            pResult.textContent =  "You Won! Rock beats Scissors";
-        } else if (e.target.classList.contains("scissors") && computerSelection === "paper") {
-            pResult.textContent =  "You won! Scissors beats Paper";
-        } else if (e.target.classList.contains("paper") && computerSelection === "rock"){
-            pResult.textContent =  "You Won! Paper beats Rock";
-        } else {
-            pResult.textContent =  `Computer Won! ${compInput} beats ${playerInput}`;
-        }     
-    
-    // Update scores
-    let computerScore = 0;
-    let userScore = 0;     
-    
+    const pointResult = document.querySelector("#pointResult")
     const divUserScore = document.querySelector("#user");
     const divComputerScore = document.querySelector("#computer");
 
-        if (e.target.classList.contains("rock") && computerSelection === "scissors" ) {
+        if( playerSelection === computerSelection){
+            pResult.textContent =  "It's a draw";
+            pointResult.textContent = "";
+        } else if (e.target.classList.contains("rock") && computerSelection === "scissors") {
+            pResult.textContent =  "You Won! Rock beats Scissors";
+            pointResult.textContent = `1 point fot ${userName}`; 
+            userScore++
+        } else if (e.target.classList.contains("scissors") && computerSelection === "paper") {
+            pResult.textContent =  "You won! Scissors beats Paper";
+            pointResult.textContent = `1 point fot ${userName}`;
             userScore++;
-        }
-        if(e.target.classList.contains("scissors") && computerSelection === "paper") {
+        } else if (e.target.classList.contains("paper") && computerSelection === "rock"){
+            pResult.textContent =  "You Won! Paper beats Rock";
+            pointResult.textContent = `1 point fot ${userName}`;
             userScore++;
-        }
-        if (e.target.classList.contains("paper") && computerSelection === "rock") {
-            userScore++;
-        }
-        if(  computerSelection === "rock" &&  e.target.classList.contains("scissors") ) {
+        } else {
+            pResult.textContent =  `Computer Won! ${compInput} beats ${playerInput}`;
+            pointResult.textContent = "1 point for Computer"
             computerScore++
         }
-        if(computerSelection === "scissors" && e.target.classList.contains("paper")) {
-            computerScore++
-        }
-        if (computerSelection === "paper" && e.target.classList.contains("rock")) {
-            computerScore++
-        }
-    
+
         divUserScore.textContent = userScore;
         divComputerScore.textContent = computerScore;
+
+        // Remove certain elements after player/computer got 5 points
+        if(computerScore === 5 || userScore === 5){
+            let removeElement = document.querySelectorAll(".remove");
+            removeElement.forEach( item => {
+                item.remove();
+            })
+            const h1 = document.createElement("h1");
+            const winnerDiv = document.querySelector(".winner");
+            h1.textContent = "Final Score";
+            winnerDiv.appendChild(h1);
+        }
+
+        const addResult = document.querySelector(".addResult");
+        if(userScore === 5){
+            addResult.textContent = `Congrats, ${userName}! You won 50 USD! `;
+            document.getElementById("playagain").style.display = "flex";
+        } else if(computerScore === 5) {
+            addResult.textContent = `Try again next time, ${userName}! Sadly, computer is better than you!`
+            document.getElementById("playagain").style.display = "flex";
+        }        
 };
-
-// Function that play a 5 round game and update the scores
-
-function game() {
-    let computerScore = 0;
-    let userScore = 0; 
-    alert("Welcome to Rock, Paper & Scissors game. This is a 5 round game. Good Luck!");
-    let name = prompt("Your Name?");
-    name = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
-    for ( let i = 1; i <= 5; i++) {
-        const playerSelection =  getUserChoice();
-        const computerSelection = getComputerChoice();;
-        const playround =  playRound(playerSelection, computerSelection);
-        if ( playerSelection === "rock" && computerSelection === "scissors" ) {
-            userScore++;
-        }
-        if(playerSelection === "scissors" && computerSelection === "paper") {
-            userScore++;
-        }
-        if (playerSelection === "paper" && computerSelection === "rock") {
-            userScore++;
-        }
-        if(  computerSelection === "rock" &&  playerSelection === "scissors" ) {
-            computerScore++;
-        }
-        if(computerSelection === "scissors" && playerSelection === "paper") {
-            computerScore++;
-        }
-        if (computerSelection === "paper" && playerSelection === "rock") {
-            computerScore++;
-        }
-        alert (`You throws ${playerSelection}.\nComputer throws ${computerSelection}.\n${playround}\n${name} - ${userScore}, Computer - ${computerScore}`);
-        console.log(`You throws ${playerSelection}.\nComputer throws ${computerSelection}.\n${playround}\n${name} - ${userScore}, Computer - ${computerScore}`);      
-    }
-   
-    if (userScore > computerScore){
-        alert (`Congrats, ${name}!\nFinal Score: You won by ${userScore} to ${computerScore}.`);
-        console.log(`Congrats, ${name}!\nFinal Score: You won by ${userScore} to ${computerScore}.`);
-    } else if (userScore === computerScore){
-        alert(`Tight game, ${name}!\nFinal Score: You share the same points with Computer by ${userScore} - ${computerScore}.`);
-        console.log(`Tight game, ${name}!\nFinal Score: You share the same points with Computer by ${userScore} - ${computerScore}.`);
-    } else {
-        alert (`Try again next time, ${name}!\nFinal Score: Computer won by ${computerScore} to ${userScore}.`);
-        console.log(`Try again next time, ${name}!\nFinal Score: Computer won by ${computerScore} to ${userScore}.`);
-    }
-    
-}
-
-// Callback function to game() function
-/* game(); */
